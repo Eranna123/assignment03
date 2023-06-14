87347df13e86eabc3943aa214a291d241aa80bce
Eranna123 <136489218+Eranna123@users.noreply.github.com>
2023-06-14 23:34:59 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,15 @@
+num1 = [1,2,3,4,5,6,7,8,9]  # series of number
+even_count = 0
+odd_count = 0
+for i in num1:
+    if i % 2 == 0:
+        even_count += 1 
+    else:
+        odd_count +=1
+print("even count is : ", even_count)  
+print("odd count is : ", odd_count)           
+
+    
+     
+
+   
